09ac5e04c3ff31c3e0566850997b41571c9a3cfb
rajmanbind <rajmanbind3535@gmail.com>
2024-05-22 17:29:20 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginante from "mongooseAggregatePaginante";
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginante);
+const Video = mongoose.model("Video", videoSchema);
+export default Video;
